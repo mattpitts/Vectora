d538a2629c775a21c0734a1e0571038c4534f2e4
Matt Pitts <mattpittsmusic@gmail.com>
2017-09-12 22:12:38 -0600

    added react-color

--- src/Components/PropertiesComponents/FillColor.js
+++ src/Components/PropertiesComponents/FillColor.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { SketchPicker } from 'react-color';
 
 const FillColor = (props) => {
 	return (
 		<div>
 			<p>Fills</p>
+			<SketchPicker
+				width={'12vw'}
+			/>
 		</div>
 	)
 }
